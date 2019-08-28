b52fbc44e211b5a459097005e85f5d9e910b6b70
Quinn Krug <quinn.krug@gmail.com>
2019-08-27 19:29:53 -0600

    reverse string complete

--- basic_algorithm_scripting/reverseString.js
+++ basic_algorithm_scripting/reverseString.js
@@ -1,5 +1,9 @@
 function reverseString(str) {
-  return str;
+  let newStr = ""
+  for (let i = str.length-1; i >= 0; i--) {
+      newStr = newStr + str[i]
+  }
+  return newStr;
 }
 
 reverseString("hello");

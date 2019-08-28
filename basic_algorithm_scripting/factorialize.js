73ab178bac54ece8919933511d11d91b760423cf
Quinn Krug <quinn.krug@gmail.com>
2019-08-27 19:39:32 -0600

    factorialize complete

--- basic_algorithm_scripting/factorialize.js
+++ basic_algorithm_scripting/factorialize.js
@@ -1,5 +1,20 @@
 function factorialize(num) {
-  return num;
+  if (num <=0)
+  {
+  return 1;
+  }
+  else
+  {
+  var n =num;
+    while(n>1)
+    {
+    n--;
+    num = num*n;
+
+    }
+
+    return num;
+  }
 }
 
 factorialize(5);

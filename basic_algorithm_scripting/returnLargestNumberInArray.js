0f204c2fbe4b478dd1b5a0fffcbac6ae6eb2b40d
Quinn Krug <quinn.krug@gmail.com>
2019-09-26 17:20:58 -0600

    return largest number in array complete

--- basic_algorithm_scripting/returnLargestNumberInArray.js
+++ basic_algorithm_scripting/returnLargestNumberInArray.js
@@ -1,6 +1,29 @@
 function largestOfFour(arr) {
-  var finalArray =[]
-  return arr;
+  var finalArray =[];
+  var n = 0;
+
+  while (arr.length>n)
+  {
+    var m = 0;
+    var biggestPiece = arr[n][m];
+    while(arr[n].length>m)
+    {
+      if (arr[n][m]>biggestPiece)
+      {
+        biggestPiece = arr[n][m];
+
+      }
+      else
+      {
+
+      }
+      m++
+    }
+    finalArray.push(biggestPiece);
+    n++;
+  }
+
+  return finalArray;
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);

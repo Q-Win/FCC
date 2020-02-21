18ade2c99af088ebf8df3f655cf137886752b44a
Quinn Krug <quinn.krug@gmail.com>
2020-02-21 16:24:45 -0700

    frakenslice 2

--- basic_algorithm_scripting/frakenSplice.js
+++ basic_algorithm_scripting/frakenSplice.js
@@ -1,6 +1,9 @@
 function frankenSplice(arr1, arr2, n) {
-  // It's alive. It's alive!
-  return arr2;
+  let finalArray = arr2.slice(0,-1);
+  let insertArray = arr1.slice(0, arr1.length);
+  finalArray.splice(n, 0, ...insertArray);
+
+return finalArray;
 }
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
